13262147a9f5df24949b263c03cf0d2ade4aabc5
KeigoTakamura <s15528@salesio-sp.ac.jp>
2019-08-12 20:43:08 +0900

    マスク処理

--- test.cpp
+++ test.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <opencv2/core.hpp>
-//#include <opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
-//#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/opencv.hpp>
-#include <opencv2/imgproc/types_c.h> // for CV_BGR2GRAY constant
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/types_c.h> 
 #include<vector>
 //#include <unistd.h>
 #include <string>
@@ -56,10 +57,12 @@ int main(int argc, const char* argv[])
    // Mat labe ;
     Mat stats;
     Mat cen;
+    double area;//塗りつぶし範囲卍
     std::vector<std::vector<Point>>imgs_points;//輪郭座標系二次元配列
    // std::vector<Vec4i> hi;
 	//double points_len; //これなに
 	image1 = imread("./ans.jpg");
+    Mat back_up = image1.clone();
     if(image1.empty()==true){
         return -1;
     }
@@ -71,21 +74,22 @@ int main(int argc, const char* argv[])
     cvtColor(image1,img2,CV_BGR2GRAY);
     img4 = img2;
     img2 = ~img2;
-    threshold(img2,img3,1.5,255,THRESH_BINARY_INV );
+    threshold(img2,img3,1.20,255,THRESH_BINARY_INV );
     GaussianBlur(img3,img3,Size(3,3),10);
     threshold(img3,img3,1.5,255,THRESH_BINARY_INV );
     medianBlur(img3,img3,7);
 
     //int n=connectedComponentsWithStats(img3,img3,stats,cen,4,CV_32S);  
     //ノイズ除去
-
-
+    
+  //  imshow("image",img3);
+    //waitKey();
     Mat labes = img3 ;//ラベリング対象画像の生成
     Mat find  = img3;
-    Mat labelImaage (labes.size(),CV_32S); //ラベリング用画像の生成
+   // Mat labelImaage (labes.size(),CV_32S); //ラベリング用画像の生成
 
     ///ラベリング実行size_
-    
+    /*
     int nlabel = connectedComponents(labes,labelImaage,8); //int nlabelがラベル数になる。
 
     std::cout << nlabel << std :: endl;
@@ -108,24 +112,30 @@ int main(int argc, const char* argv[])
         }
         
     }
+    */
+   Mat output (img3.size(),CV_8UC3);//ラベリング結果出力
+
     
-    Mat sabun;
-    cvtColor(output,output,CV_BGR2GRAY);
-    threshold(output,output,20,255,THRESH_BINARY );
-    morphologyEx(output, output,MORPH_OPEN, Mat(), Point(-1, -1), 1);
+   // Mat sabun_bg;//膨張後差分
+    output = img3;
+    //cvtColor(img3,output,CV_BGR2GRAY);
+    //threshold(output,output,20,255,THRESH_BINARY );
+    morphologyEx(output, output,MORPH_CLOSE, Mat(), Point(-1, -1), 1);
+    morphologyEx(output, output,MORPH_DILATE, Mat(), Point(-1, -1), 1);
    
-    
-    
-
-
-
     //imshow("a",output);
     //waitKey();
-    /*
+    
     std :: vector<std::vector<Point>> contuors;
     std :: vector <Vec4i> he;
     
-    findContours(binarys,contuors,he,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_TC89_L1);
+    imshow("a",output);
+    waitKey();
+
+    findContours(output,contuors,he,CV_RETR_EXTERNAL ,CV_CHAIN_APPROX_NONE );
+ 
+ //輪郭四角表示部
+ /*
     Point MinP;
     Point MaxP;
     for (int i = 0; i < contuors.size(); i++)
@@ -138,17 +148,36 @@ int main(int argc, const char* argv[])
         rectangle( image1, rect, Scalar(0,255,0),2,8);
     }
     
-    std::cout << contuors.size()  << std :: endl;
-    
-    
-    cv::namedWindow("Source", cv::WINDOW_AUTOSIZE );
-    cv::imshow("Source", image1);
-    waitKey();
-    return 0;
-    */
+    //std::cout << contuors.size()  << std :: endl;
 
-    cv::namedWindow("f", WINDOW_AUTOSIZE );
-    cv::imshow("f", output);
+*/
+
+    std::vector<std::vector<Point> > contours_subset;
+    
+    for (long unsigned int i = 0; i < contuors.size(); i++)//マスク処理
+    {
+        area = contourArea(contuors.at(i));
+        printf("%f\n",area);
+        if(area > 1000){
+            contours_subset.push_back(contuors.at(i));//面積情報でわける
+        }
+    }
+    
+    
+    Mat mask_data = Mat::zeros(image1.rows, image1.cols, CV_8UC3);
+    
+
+    drawContours(mask_data,contours_subset,-1,Scalar(255,255,255),-1);
+  
+
+
+    cv::namedWindow("Source", cv::WINDOW_AUTOSIZE );
+    cv::imshow("Source", mask_data);
     waitKey();
+        
+
+   // cv::namedWindow("f", WINDOW_AUTOSIZE );
+   // cv::imshow("f", sabun_bg);
+    //waitKey();
  return 0;
 }
